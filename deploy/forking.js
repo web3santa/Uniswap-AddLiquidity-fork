abdb93d1e0cf8137ea371774ea989dd253e704b9
web3santa <usefulstory35@gmail.com>
2024-06-29 18:59:38 +0900

    added

---
+++ deploy/forking.js
@@ -0,0 +1,18 @@
+const { parseEther, parseUnits, MaxUint256, getBalance } = require("ethers");
+const { ethers } = require("hardhat");
+
+const ERC20ABI = require('../ERC20.json');
+
+
+const main = async () => {
+  
+
+
+  
+
+};
+
+main().catch((e) => {
+  console.log(e);
+  process.exit(1);
+});
